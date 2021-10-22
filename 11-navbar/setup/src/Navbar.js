4b22e595e670fc1a99150f964fdc7af0320afa2d
luis_mendiola@upperdeck.com <luis_mendiola@upperdeck.com>
2021-10-21 17:01:20 -0700

    second commit

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,41 @@
+// eslint-disable-next-line
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
+import { FaBars, FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
+// eslint-disable-next-line
 import { links, social } from './data'
+// eslint-disable-next-line
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return <nav><div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo" />
+        <button className="nav-toggle">
+          <FaBars/>
+        </button>
+      </div>
+      <div className="links-container show-container">
+        <ul className="links">
+          {links.map((link)=> {
+            const {id, url, text} = link;
+            return (<li key={id}>
+                <a href={url}>{text}</a>
+              </li>)
+          })}
+        </ul>
+      </div>
+      <ul className="social-icons">
+        {social.map((profile) => {
+          const {id, url, icon} = profile;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
+      </div>
+    </nav>
 }
 
 export default Navbar
--- 11-navbar/setup/src/data.js
+++ 11-navbar/setup/src/data.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { FaBehance, FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
 export const links = [
   {
     id: 1,
@@ -41,12 +41,12 @@ export const social = [
   },
   {
     id: 3,
-    url: 'https://www.twitter.com',
+    url: 'https://www.linkedin.com',
     icon: <FaLinkedin />,
   },
   {
     id: 4,
-    url: 'https://www.twitter.com',
-    icon: <FaBehance />,
+    url: 'https://www.instagram.com',
+    icon: <FaInstagram />,
   },
 ]
